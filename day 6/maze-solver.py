6f3de35d3013813748201cf0a8820a09814ef8bd
Aryan Khandelwal <aryankhandelwal15@gmail.com>
2025-10-28 13:59:49 +0530

    added day 6 maze solver

---
+++ day 6/maze-solver.py
@@ -0,0 +1,21 @@
+# this is solution to the below maze
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+think(0)
+
+while not at_goal():
+    if not front_is_clear() and not right_is_clear():
+        turn_left()
+    elif front_is_clear() and not right_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
